a854a4986b13d41c17cfe19bf5e53aa5ffc924c8
Artem Kravtsov <dr.artem-k@yandex.com>
2020-07-30 21:01:05 +0400

    edit app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable eol-last */
 const express = require('express');
 const { PORT = 3000 } = process.env;
@@ -10,5 +11,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Слушаю порт ${PORT}`);
+});
